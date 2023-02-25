a3db8710a1e6f803b0c4479abe3d50f4c00c86bf
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2023-02-25 20:44:01 +0530

    Worked with basic functions in OpenCV

---
+++ basic.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+
+img=cv.imread('Photos/park.jpg')
+cv.imshow('Park',img)
+
+#Converting image to Gray Scale Image
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('Gray Image',gray)
+
+#Blur
+#ksize is assigned always an odd value
+blur=cv.GaussianBlur(img,(7,7),cv.BORDER_DEFAULT)
+cv.imshow('Blur Image',blur)
+
+#Edge Cascades
+canny=cv.Canny(blur,125,175)
+cv.imshow('Edge Cascaded Image',canny)
+
+#Dialating the image
+dilated=cv.dilate(canny,(7,7),iterations=3)
+cv.imshow('Dilated Image',dilated)
+
+#Eroding Image
+eroded=cv.erode(dilated,(7,7),iterations=3)
+cv.imshow('Eroded Image',eroded)
+
+#Resize Image
+resized=cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized Image',resized)
+
+#Cropping Image
+cropped=img[50:200,200:400]
+cv.imshow('Cropped Image',cropped)
+
+cv.waitKey(0)
